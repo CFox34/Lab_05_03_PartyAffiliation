8746132235d685b03543d81b1efd9a6a05adab0a
Colton Fox <coltonfox@coltons-mbp.wifi.uc.edu>
2022-09-22 14:49:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String party = "I";
+
+        if (party.equals("R"))
+        {
+            System.out.println("You get a republican elephant!");
+        }
+        else if (party.equals("D"))
+        {
+            System.out.println("You get a democratic donkey!");
+        }
+        else
+        {
+            System.out.println("You get an independent person!");
+        }
+    }
+}
